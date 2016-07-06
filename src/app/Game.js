8c6297d69da604a3a75251048526500ad75a4d24
xlionheart <my@mengyangdeMacBook-Air.local>
2016-07-06 23:22:21 +0800

    tempfile feat/move

--- src/app/Game.js
+++ src/app/Game.js
@@ -1,36 +1,35 @@
 import Sheep from './Sheep.js';
 
 export default class Game {
-  constructor(ctx) {
-    this.ctx = ctx;
-    this.drawables = [];
-    this.drawables.push(new Sheep(ctx, [10, 10]));
-  }
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.drawables = [];
+        this.drawables.push(new Sheep(ctx, [10, 10]));
+    }
 
-  run() {
-    //this.init();
-    requestAnimationFrame(function step() {
-      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-      this.ctx.save();
-      this.ctx.fillStyle = '#F8F8F8';
-      this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-      this.ctx.restore();
+    run() {
+        this.keyBinding(); 
+        requestAnimationFrame(function step() {
+            this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+            this.ctx.save();
+            this.ctx.fillStyle = '#F8F8F8';
+            this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+            this.ctx.restore();
 
-      this.drawables.forEach(drawable => {
-        drawable.update();
-        drawable.draw();
-      });
-      requestAnimationFrame(step.bind(this));
-    }.bind(this));
-  }
-  // handleEvent(){
-  //   if (event.type ==='keydown'&&event.key ===' ' ) {
-  //     if(this.drawables[0].canJump()){
-  //       this.drawables[0].jump();
-  //     }
-  //   }
-  // };
-  // init() {
-  //   window.addEventListener('keydown',this,false);
-  // };
+            this.drawables.forEach(drawable => {
+                drawable.update();
+                drawable.draw();
+            });
+            requestAnimationFrame(step.bind(this));
+        }.bind(this));
+    }
+
+    keyBinding() {
+            window.addEventListener('keydown', e => {
+                this.drawables[0].action(e);
+            });
+            window.addEventListener('keyup', e => {
+                this.drawables[0].action(e);
+            });
+        }
 }
--- src/app/Sheep.js
+++ src/app/Sheep.js
@@ -1,75 +1,161 @@
 export default class Sheep {
-  constructor(ctx, position) {
-    this.ctx = ctx;
-    this.position = position || [0, 0];
-    this.dimension = [36, 36];
-    this.g = 0.2; // NOTE: Gravity.
-    this.v = [0, 0]; // NOTE: Velocity.
-    this.jumpV = 7;
-    this.jumpA = [0.2,0.1];
-    window.addEventListener('keydown',this,false);
-    window.addEventListener('keyup',this,false);
-  }
-  handleEvent(){
-    if (event.type ==='keydown'&&event.key ===' ' ) {
-        this.jump();
-    }
-    if (event.type ==='keyup'&&event.key ===' ' ) {
-        this.endJump();
-    }
-  };
-  update() {
-    this.fall();
-  }
+    constructor(ctx, position) {
+        this.ctx = ctx;
+        this.position = position || [0, 0];
+        this.dimension = [36, 36];
+        this.velocity = [0, 0]; //速度
+        this.acceleration = [0, 1]; // 加速度
 
-  /**
-   * Simple collision check.
-   * @return {boolean} whether can the sheep fall.
-   */
-  canFall() {
-    return this.position[1] + this.dimension[1] + 1 < this.ctx.canvas.height;
-  }
-  /**
-   * Simple collision check.
-   * @return {boolean}  Can the sheep jump.
-   */
-  canJump() {
-    return (this.position[1] + this.dimension[1] + 1 >= this.ctx.canvas.height)&&(this.position[1] - 1 > 0);
-  }
-  /**
-   * Simple collision check.
-   * @return {boolean}  IS the sheep on the ground.
-   */
-  isOnTheGround(){
-    return this.position[1] + this.dimension[1] + 1 >= this.ctx.canvas.height;
-  }
-  /**
-   * Sheep fall.
-   */
-  fall() {
-    this.position[1] = this.position[1] + this.v[1] + this.g/2;
-    if(this.isOnTheGround()){
-      this.position[1] = this.ctx.canvas.height - this.dimension[1] - 1;
-      this.v[1] = 0;
-    } else {
-      this.v[1] += this.g;
+        this.jumpV = 14; //跳跃初始速度
+        this.initialV = 7; //横向初始速度
+        this.jumpA = [1, 0.3]; //默认重力加速度,跳跃按下时重力加速度
     }
-  }
-  // TODO: When calling this method, the sheep will `jump`.
-  jump() {
-    if(this.isOnTheGround()) {
-      this.v[1] -= this.jumpV;
+
+    update() {
+        this.move();
     }
-    this.g =this.jumpA[1];
-  }
-  // TODO: When calling this method, the sheep will `endjump`.
-  endJump() {
-    this.g =this.jumpA[0];
-  }
-  draw() {
-    this.ctx.save();
-    this.ctx.strokeStyle = 'red';
-    this.ctx.strokeRect(this.position[0], this.position[1], this.dimension[0], this.dimension[1]);
-    this.ctx.restore();
-  }
+
+    /*
+     *越界检测,并且强制处理
+     */
+    borderDetect() {
+        if (this.position[0] < 0) {
+            this.position[0] = 1
+        } else if (this.position[0] > this.ctx.canvas.width - this.dimension[1]) {
+            this.position[0] = this.ctx.canvas.width - this.dimension[1] - 1
+        }
+
+        if (this.position[1] > this.ctx.canvas.height - this.dimension[1]) {
+            this.position[1] = this.ctx.canvas.height - this.dimension[1] - 1
+        }
+    }
+
+    /*
+     *判断是否在地面
+     */
+    isOnTheGround() {
+        return this.position[1] + this.dimension[1] + 1 >= this.ctx.canvas.height;
+    }
+
+    /**
+     * 水平移动计算
+     */
+    horizontalMove() {
+        this.position[0] = this.position[0] + this.velocity[0] + this.acceleration[0] / 2;
+        this.velocity[0] = this.velocity[0] + this.acceleration[0]
+        return this.position[0]
+    }
+
+    /**
+     * 垂直移动计算
+     */
+    VerticalMove() {
+        this.position[1] = this.position[1] + this.velocity[1] + this.acceleration[1] / 2;
+        this.velocity[1] = this.velocity[1] + this.acceleration[1]
+        return this.position[1]
+    }
+
+    /**
+     * 计算位移,修改坐标
+     */
+    move() {
+        this.horizontalMove();
+        this.VerticalMove();
+        this.borderDetect();
+    }
+
+
+    /*
+     *行动
+     */
+    action(e) {
+        console.log(e.type);
+        console.log(e.code);
+        console.log(e.key);
+        console.log(e.repeat);
+        if (e.type == "keydown") {
+            switch (e.code) {
+                case "Space":
+                    this.jump()
+                    break;
+                case 'KeyD':
+                    this.moveRight(e.repeat);
+                    break;
+                case 'KeyA':
+                    this.moveLeft(e.repeat);
+                    break;
+                default:
+            }
+        } else if (e.type == 'keyup') {
+            switch (e.code) {
+                case "Space":
+                    this.endJump()
+                    break;
+                case 'KeyD':
+                    this.endMoveRight();
+                    break;
+                case 'KeyA':
+                    this.endMoveLeft();
+                    break;
+                default:
+            }
+        }
+    }
+
+    /**
+     * 跳跃
+     */
+    jump() {
+        if (this.isOnTheGround()) {
+            this.velocity[1] = -this.jumpV;
+        }
+        this.acceleration[1] = this.jumpA[1];
+    }
+
+    /**
+     * 跳跃状态结束
+     */
+    endJump() {
+        this.acceleration[1] = this.jumpA[0];
+    }
+
+    /**
+     * 向右移动
+     */
+    moveRight(repeat) {
+        if (repeat === false) {
+            this.velocity[0] = this.velocity[0] + this.initialV;
+        }
+    }
+
+    /**
+     * 向左移动
+     */
+    moveLeft(repeat) {
+        if (repeat === false) {
+            this.velocity[0] = this.velocity[0] - this.initialV;
+        }
+    }
+
+    /**
+     * 停止向右移动
+     */
+    endMoveRight() {
+        this.velocity[0] = this.velocity[0] - this.initialV;
+    }
+
+    /**
+     * 停止向左移动
+     */
+    endMoveLeft() {
+        this.velocity[0] = this.velocity[0] + this.initialV;
+    }
+
+
+    draw() {
+        this.ctx.save();
+        this.ctx.strokeStyle = 'red';
+        this.ctx.strokeRect(this.position[0], this.position[1], this.dimension[0], this.dimension[1]);
+        this.ctx.restore();
+    }
 }
